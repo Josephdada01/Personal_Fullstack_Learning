61dc33341c2b8e57412ea353b2b4d579ff8dfe5b
Josephdada01 <ridwandada500@gmail.com>
2023-12-12 17:41:42 +0100

    task 1

---
+++ javascript/add7.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * a function that takes in number and returns the number + 7
+ */
+function add7(number) {
+    result = number + 7
+    return result
+}
+console.log(add7(10))
+console.log(add7(20))
