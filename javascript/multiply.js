ddf97dbbc3bd718e9edd50b73e783ef1ec19365b
Josephdada01 <ridwandada500@gmail.com>
2023-12-12 17:52:26 +0100

    multiply

---
+++ javascript/multiply.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * a function called multiply that takes 2 numbers and returns their product.
+ */
+function multiply(a, b) {
+    return a * b
+}
+console.log(multiply(10, 20))
